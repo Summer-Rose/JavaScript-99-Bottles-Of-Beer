10f0041b2502349b8ef5f57e7ba453f51bbb2780
Summer Brochtrup <sbrochtrup@gmail.com>
2015-09-18 10:34:49 -0700

    Add spec to test that bottlesOfBeer returns an array length of 3

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
-describe('functionName', function() {
-  it("describe behavior", function() {
-    expect(functionName(parameter)).to.equal(result);
+describe('bottlesOfBeer', function() {
+  it("returns song with number of lines corresponding to inputted number", function() {
+    expect(bottlesOfBeer(2)).to.have.length(3);
   });
 });
+
+
+//(["2 bottles of beer on the wall, 2 bottles of beer. Take one down, pass it around, 1 bottle of beer on the wall.", "1 bottle of beer on the wall, 1 bottle of beer. Take one down, pass it around, 0 bottles of beer on the wall.", "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."]);
